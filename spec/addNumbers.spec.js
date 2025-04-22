a367348ab1de38da4e84d0a8b6d6cb5a9c15c350
Manish Silwal <silwalmanees@gmail.com>
2025-04-22 21:36:54 +0545

    create a jasmine test

---
+++ spec/addNumbers.spec.js
@@ -0,0 +1,20 @@
+import { addNumbers } from "../src/addNumbers.js";
+
+describe("test suite", function(){
+    it("works with positive numbers", function(){
+        expect(addNumbers(1,2)).toEqual(3);
+    });
+
+    it("works with negative numbers", function(){
+        expect(addNumbers(-1,-2)).toEqual(-3);
+    });
+
+    it("works with zero", function(){
+        expect(addNumbers(0,2)).toEqual(2);
+    });
+
+    it("works with positive and negative", function(){
+        expect(addNumbers(2,-3)).toEqual(-1);
+    });
+});
+
